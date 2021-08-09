fb711c3c4af6bbcfde89eddf4e1b35132360fca6
povijarrro <povijarrro@gmail.com>
2021-08-09 10:41:27 +0200

    some changes

--- pygametest/pygametest.py
+++ pygametest/pygametest.py
@@ -1,16 +1,26 @@
 import pygame as pg
+from pygame.constants import K_ESCAPE
 
 def init(bg):
     pg.init()
-    bgrnd=pg.image.load(bg)
+    bgrnd=pg.image.load("pygametest/"+bg)
     bg_size=bgrnd.get_size()
-    window=pg.display.set_mode((900*bg_size[0]/bg_size[1],900))
+    bgrnd=pg.transform.scale(bgrnd,(int(900*bg_size[0]/bg_size[1]), 900))
+    bg_size = bgrnd.get_size()
+    window = pg.display.set_mode(bg_size)
+    window.blit(bgrnd,(0,0))
     pg.display.update()
 
 init("bgrnd.jpg")
+car = pg.image.load("pygametest/car-5.png")
+window = pg.display.get_surface()
+size=window.get_size()
+car_size=car.get_size()
+window.blit(car,(int(size[0]/2-car_size[0]/2),size[1]-250))
+pg.display.update()
 
 running = True
 while running:
-  for event in pg.event.get():
-    if event.type == pg.QUIT:
-      running = False
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
