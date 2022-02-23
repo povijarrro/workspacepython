6370bf294ac78ec631f62ce1f31de5daa5cfe9f4
povijarrro <povijarrro@gmail.com>
2022-02-23 06:13:56 +0100

    improve egyptian

--- egyptian.py
+++ egyptian.py
@@ -1,18 +1,23 @@
 from math import gcd
 
-def egyptian(x,y):
+def egyptian(x, y):
     g=gcd(x,y)
-    neg=False
-    if((x<0)^(y<0)):neg=True
+    neg=((x<0)^(y<0))
     x=abs(x//g)
     y=abs(y//g)
+    first=""
+    if(x>y):
+        div=x//y
+        first=str(div)
+        x=x-y*div
+        return(first+"+"+egyptian(x,y))
     if(x==1): return("1/"+str(y))
     if(x==0): return("0")
     if(y==0): return("UNDEFINED")
     c=y//x+1
-    composition="1/"+str(c)+"+"+egyptian((-y)%x,y*c)
+    composition='1/'+str(c)+'+'+egyptian((-y)%x, y*c)
     if(neg):
-        composition="-"+composition.replace("+","-")
+        composition="-"+composition.replace('+', '-')
     return(composition)
 
-print(egyptian(41,43))
+print(egyptian(23, 7))
