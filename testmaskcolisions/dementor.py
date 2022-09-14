7e010295fb581563c641bc0813b6a35a7486a7f0
povijarrro <povijarrro@gmail.com>
2022-09-14 09:33:12 +0200

    added mask test

---
+++ testmaskcolisions/dementor.py
@@ -0,0 +1,48 @@
+#!python
+import pygame as pg
+
+SIZE = 800,800
+
+pg.init()
+
+dis = pg.display.set_mode(SIZE)
+dis.fill((255,255,255))
+clock=pg.time.Clock()
+
+dementor_img=pg.image.load("dementor.png").convert_alpha()
+dementor_rect=dementor_img.get_rect()
+dementor_rect.center=SIZE[0]/2, SIZE[1]/2
+dementor_mask=pg.mask.from_surface(dementor_img)
+
+
+
+point=pg.Surface((1,1)).convert_alpha()
+point.fill((0,0,0))
+point_rect=point.get_rect()
+
+dis.blit(dementor_img, dementor_rect)
+pg.display.update()
+
+run = True
+i=0
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run=False
+
+            
+        if event.type == pg.MOUSEBUTTONDOWN:
+            point_rect.topleft=pg.mouse.get_pos()
+            m=pg.mask.from_surface(point)
+
+            i+=1
+            if dementor_mask.overlap(m,(point_rect.left-dementor_rect.left, point_rect.top-dementor_rect.top)):
+                print(True,f"{i}")
+            else:
+                print(False,f"{i}")    
+    
+    pg.display.update()
+    clock.tick(60)            
+
+
+pg.quit()
