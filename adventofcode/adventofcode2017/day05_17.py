d2b96fafd36b8d81528781055cfb9bc14d2055e1
povijarrro <povijarrro@gmail.com>
2024-03-07 07:50:56 +0100

    added day 05_17

---
+++ adventofcode/adventofcode2017/day05_17.py
@@ -0,0 +1,32 @@
+#!python
+
+def sol(instructions:list[int],part=1)->int:
+    ins = instructions.copy()
+    if part == 1 :
+        i = 0
+        steps = 0
+        while i<len(ins):
+            ins[i] += 1
+            i += ins[i]-1
+            steps += 1
+
+    else:
+        i = 0
+        steps = 0
+        while i<len(ins):
+            if ins[i]<3 :
+                ins[i] += 1
+                i += ins[i]-1
+            else:
+                ins[i] -= 1
+                i += ins[i]+1    
+            steps += 1
+
+    return steps
+
+
+if __name__ == "__main__":
+    with open("input05_17.txt") as inp:
+        instructions = [int(d.strip()) for d in inp.readlines()]
+
+    print(f"Part 1 : {sol(instructions)}\nPart 2 : {sol(instructions,2)}")
