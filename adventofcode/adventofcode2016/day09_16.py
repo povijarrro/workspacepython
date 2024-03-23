582fc6703b0c631782096bd11faa240486407702
povijarrro <povijarrro@gmail.com>
2024-03-23 07:54:01 +0100

    complete day 09_16

--- adventofcode/adventofcode2016/day09_16.py
+++ adventofcode/adventofcode2016/day09_16.py
@@ -1,24 +1,21 @@
 #!python
-import sys
-sys.setrecursionlimit(100000)
-def get_real_seq(sequence:str, part = 1)->str:
-    if "(" not in sequence:
-        return sequence
+def get_real_len(sequence:str, start,end, part = 1)->int:
+    n = end-start
+    if "(" not in sequence[start:end]:
+        return n
     else:
-        ilpar = sequence.index("(")
-        irpar = sequence.index(")")
+        ilpar = sequence.find("(",start,end)
+        irpar = sequence.find(")",ilpar+1)
         spl = sequence[ilpar+1:irpar].split("x")
-        real_seq = sequence[:ilpar]+int(spl[1])*sequence[irpar+1:irpar+1+int(spl[0])]+get_real_seq(sequence[irpar+1+int(spl[0]):])
+        real_len = ilpar+int(spl[1])*int(spl[0])+get_real_len(sequence[irpar+1+int(spl[0]):],0,n-irpar-1-int(spl[0]))
         if part == 1:
-            return real_seq
+            return real_len
         else:
-            real_seq = sequence
-            while "(" in real_seq:
-                real_seq = get_real_seq(real_seq)
-            return real_seq    
+            real_len = ilpar - start + int(spl[1])*get_real_len(sequence,irpar+1,irpar+1+int(spl[0]),part)+get_real_len(sequence,irpar+1+int(spl[0]),end,part)
+            return real_len 
 
 def sol(sequence:str, part = 1)->int:
-    return len(get_real_seq(sequence,part))
+    return get_real_len(sequence,0,len(sequence),part)
 
 
         
@@ -26,6 +23,4 @@ if __name__ == "__main__":
     with open("input09_16.txt") as inp:
         sequence = inp.readline().strip()
 
-    print(sol(sequence,2))     
-
-    
+    print(sol(sequence,2))
