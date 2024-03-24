b9fe95c4cc004162ff2433e42e24ec3567f32805
povijarrro <povijarrro@gmail.com>
2024-03-24 09:35:15 +0100

    complete day 10_16

--- adventofcode/adventofcode2016/day10_16.py
+++ adventofcode/adventofcode2016/day10_16.py
@@ -12,7 +12,7 @@ def set_chips(instructions:list[str])->dict:
         
     return res
 
-def changed(bots:dict, instructions:list[str])->dict:
+def changed(bots:dict, instructions:list[str],output:dict)->dict:
     cbots = bots.copy()
     two_valued = [bot for bot in bots if isinstance(bots[bot],dict)][0]
     for ins in instructions:
@@ -28,26 +28,39 @@ def changed(bots:dict, instructions:list[str])->dict:
                 if spl[5] == "bot" :
                     cbots[spl[6]] = bots[two_valued]["low"] if spl[6] not in bots else {"low":min(bots[two_valued]["low"],bots[spl[6]]),
                                                                                         "high":max(bots[two_valued]["low"],bots[spl[6]])}
-                    cbots[two_valued] = bots[two_valued]["high"]   
+                    cbots[two_valued] = bots[two_valued]["high"]
+                    output.update({spl[-1]:bots[two_valued]["high"]})
+                       
                 else:
                     cbots[spl[-1]] = bots[two_valued]["high"] if spl[-1] not in bots else {"low":min(bots[two_valued]["high"],bots[spl[-1]]),
                                                                                            "high":max(bots[two_valued]["high"],bots[spl[-1]])}
                     cbots[two_valued] = bots[two_valued]["low"]
+                    output.update({spl[6]:bots[two_valued]["low"]})
+                    if spl[-2] == "output":
+                        output.update({spl[-1]:bots[two_valued]["high"]})
+
             cbots.pop(two_valued,None)
-    return cbots 
+    return cbots,output
 
 def sol(instructions:list[str],d,part=1)->str:
     bots = set_chips(instructions)
-    if d in bots.values():
-        return [bot for bot in bots if bots[bot]==d][0]
-    while d not in bots.values():
-        bots=changed(bots,instructions)
+    output = {}
+    if part == 1:
         if d in bots.values():
             return [bot for bot in bots if bots[bot]==d][0]
+        while d not in bots.values():
+            bots,_ = changed(bots,instructions,output)
+            if d in bots.values():
+                return [bot for bot in bots if bots[bot]==d][0]
+    else:
+        while "0" not in output or "1" not in output or "2" not in output:
+            bots,output = changed(bots,instructions,output)
+            if "0" in output and "1" in output and "2" in output:
+                return output["0"]*output["1"]*output["2"]       
 
 
 if __name__ == "__main__":
     with open("input10_16.txt") as inp:
         instructions = [d.strip() for d in inp.readlines()]
     
-    print(sol(instructions,{"low":17,"high":61}))
+    print(f"Part 1 : {sol(instructions,{"low":17,"high":61})}\nPart 2 : {sol(instructions,None,2)}")
