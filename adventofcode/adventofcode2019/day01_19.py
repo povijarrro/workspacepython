6ce89801593aba8655c80cb0de5ef3ab368dc45b
povijarrro <povijarrro@gmail.com>
2024-04-29 10:01:01 +0200

    added day01_19

---
+++ adventofcode/adventofcode2019/day01_19.py
@@ -0,0 +1,19 @@
+#!python
+def fuel(n:int,part = 1)->int:
+    if part == 1:
+        return max(0,n//3 - 2)
+    else:
+        m = n//3 -2
+        if m<=0:
+            return 0
+        else:
+            return m +fuel(m,part)
+
+def sol(data:list[int],part = 1)->int:
+    return sum([fuel(d,part) for d in data])
+
+if __name__ == "__main__":
+    with open("input01_19.txt") as inp:
+        data = [int(d.strip()) for d in inp.readlines()]
+    
+    print(f"Part 1 : {sol(data)}\nPart 2 : {sol(data,2)}")    
