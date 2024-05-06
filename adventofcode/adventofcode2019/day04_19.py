a076d1e5b8e6f8af5adf7b12808dea00dc4237a6
povijarrro <povijarrro@gmail.com>
2024-05-06 10:59:11 +0200

    added day04_19

---
+++ adventofcode/adventofcode2019/day04_19.py
@@ -0,0 +1,37 @@
+#!python
+def get_counts(s:str)->list[int]:
+    counts=[1]
+    for i in range(len(s)-1):
+        if s[i]!=s[i+1]:
+            counts.append(1)
+        else:
+            counts[-1]+=1    
+    return counts
+
+def has_double(s:str,part = 1)->bool:
+    gc = get_counts(s)
+    return any(c>=2 for c in gc) if part == 1 else any(c==2 for c in gc)
+        
+def is_nondecreasing(s:str)->bool:
+    for i in range(len(s)-1):
+            if ord(s[i+1])<ord(s[i]):
+                return False
+    return True
+
+def get_passwords(low:int,high:int, part = 1)->set[str]:
+    passwords = set()
+    for i in range(max(low,100000),min(high,1000000)):
+        if (not has_double(str(i),part) or (not is_nondecreasing(str(i)))):
+            continue
+        passwords.add(str(i))
+    return passwords 
+
+def sol(low:int,high:int,part = 1):
+    return len(get_passwords(low,high,part)) 
+
+
+
+if __name__ == "__main__":
+    low,high = 172851,675869
+    
+    print(f"Part 1 : {sol(low,high)}\nPart 2 : {sol(low,high,2)}")
